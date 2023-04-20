8fb48d704925c6e840eadaf35270d0db75ccf92a
adnane <adnanbenzo194@gmail.com>
2023-04-20 00:08:19 +0000

    Revert "Edit db_design & Implement all  db entities + dtos"
    This reverts commit f9ce69de3d1074a7c203ceefb9c755b845e99950.

--- src/dtos/admins.dtos.ts
+++ src/dtos/admins.dtos.ts
@@ -1,13 +1,11 @@
-import { IsNotEmpty, IsString, MinLength } from 'class-validator';
+import { IsNotEmpty, MinLength } from 'class-validator';
 
 export class CreateAdminDto {
   @IsNotEmpty()
   @MinLength(3)
-  @IsString()
   username: string;
 
   @IsNotEmpty()
   @MinLength(8)
-  @IsString()
   password: string;
 }
--- src/dtos/resumes.dtos.ts
+++
@@ -1,7 +0,0 @@
-import { IsNotEmpty, IsString } from 'class-validator';
-
-export class CreateResumeDto {
-  @IsNotEmpty()
-  @IsString()
-  pdf: string;
-}

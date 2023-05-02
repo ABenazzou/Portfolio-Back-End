f7d345ae70f75be77692d5619f46aed150b446e9
adnane <adnanbenzo194@gmail.com>
2023-05-02 23:15:59 +0100

    Remove hello world method/controller @ root route /

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { SectionsModule } from './sections/sections.module';
@@ -40,7 +38,7 @@ import { AuthModule } from './auth/auth.module';
     ProjectsModule,
     AuthModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}

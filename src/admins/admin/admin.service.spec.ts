9a0596cd0a0b0e10d490a2144e4f3cb9246248cb
adnane <adnanbenzo194@gmail.com>
2023-05-09 23:18:26 +0100

    Initiated Unit Tests (Admin Service)

--- src/admins/admin/admin.service.spec.ts
+++ src/admins/admin/admin.service.spec.ts
@@ -1,18 +1,36 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AdminService } from './admin.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Admin } from 'src/entities/admin';
+import { Repository } from 'typeorm';
 
 describe('AdminService', () => {
   let service: AdminService;
+  let adminRepository: Repository<Admin>;
+  const ADMIN_REPOSITORY_TOKEN = getRepositoryToken(Admin);
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AdminService],
+      providers: [
+        AdminService,
+        {
+          provide: ADMIN_REPOSITORY_TOKEN,
+          useValue: {
+            findOne: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<AdminService>(AdminService);
+    adminRepository = module.get<Repository<Admin>>(ADMIN_REPOSITORY_TOKEN);
   });
 
-  it('should be defined', () => {
+  it('service should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('adminRepository should be defined', () => {
+    expect(adminRepository).toBeDefined();
   });
 });

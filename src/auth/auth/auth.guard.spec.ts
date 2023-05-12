c57f5bd1e46e64735011d8e81d36f9a05cb4b133
adnane <adnanbenzo194@gmail.com>
2023-05-12 22:43:41 +0100

    Resolved initial unit test dependencies for auth module

--- src/auth/auth/auth.guard.spec.ts
+++ src/auth/auth/auth.guard.spec.ts
@@ -1,7 +1,19 @@
+import { JwtSecretRequestType, JwtService } from '@nestjs/jwt';
 import { AuthGuard } from './auth.guard';
+import { ConfigService } from '@nestjs/config';
 
 describe('AuthGuard', () => {
+  let authGuard: AuthGuard;
+  let jwtService: JwtService;
+  let configService: ConfigService;
+
+  beforeEach(async () => {
+    jwtService = new JwtService();
+    configService = new ConfigService();
+    authGuard = new AuthGuard(jwtService, configService);
+  });
+
   it('should be defined', () => {
-    expect(new AuthGuard()).toBeDefined();
+    expect(authGuard).toBeDefined();
   });
 });
--- src/auth/auth/auth.service.spec.ts
+++ src/auth/auth/auth.service.spec.ts
@@ -1,18 +1,37 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { AdminService } from 'src/admins/admin/admin.service';
+import { JwtService } from '@nestjs/jwt';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let controller: AuthController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: AdminService,
+          useValue: {
+            findOne: jest.fn(),
+          },
+        },
+        JwtService,
+      ],
+      controllers: [AuthController],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
+    controller = module.get<AuthController>(AuthController);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('controller should be defined', () => {
+    expect(controller).toBeDefined();
   });
 });

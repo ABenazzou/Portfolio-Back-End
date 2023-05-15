d3df5e84720b0fc2a2b72ee5e007ee7517d7498f
adnane <adnanbenzo194@gmail.com>
2023-05-15 22:53:00 +0100

    Resolve dependencies for hobbies module unit tests

--- src/hobbies/hobby/hobby.controller.spec.ts
+++ src/hobbies/hobby/hobby.controller.spec.ts
@@ -1,11 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { HobbyController } from './hobby.controller';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { HobbyService } from './hobby.service';
 
 describe('HobbyController', () => {
   let controller: HobbyController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        JwtService,
+        ConfigService,
+        {
+          provide: HobbyService,
+          useValue: {
+            getHobbies: jest.fn(),
+            createHobby: jest.fn(),
+            getHobbyById: jest.fn(),
+            updateHobby: jest.fn(),
+            deleteHobby: jest.fn(),
+          },
+        },
+      ],
       controllers: [HobbyController],
     }).compile();
 
--- src/hobbies/hobby/hobby.service.spec.ts
+++ src/hobbies/hobby/hobby.service.spec.ts
@@ -1,18 +1,43 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { HobbyService } from './hobby.service';
+import { Repository } from 'typeorm';
+import { Hobby } from 'src/entities/hobby';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { JwtService } from '@nestjs/jwt';
 
 describe('HobbyService', () => {
   let service: HobbyService;
+  let hobbyRepository: Repository<Hobby>;
+  const HOBBY_REPOSITORY_TOKEN = getRepositoryToken(Hobby);
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [HobbyService],
+      providers: [
+        HobbyService,
+        JwtService,
+        {
+          provide: HOBBY_REPOSITORY_TOKEN,
+          useValue: {
+            find: jest.fn(),
+            save: jest.fn(),
+            create: jest.fn(),
+            findOneBy: jest.fn(),
+            update: jest.fn(),
+            delete: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<HobbyService>(HobbyService);
+    hobbyRepository = module.get<Repository<Hobby>>(HOBBY_REPOSITORY_TOKEN);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('hobbyRepository should be defined', () => {
+    expect(hobbyRepository).toBeDefined();
   });
 });

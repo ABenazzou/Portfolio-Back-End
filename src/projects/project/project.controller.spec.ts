d0fb1921249749e7797e6964ee9d36cbb72c1915
adnane <adnanbenzo194@gmail.com>
2023-05-16 23:10:43 +0100

    Updated projects module unit tests to handle dependencies (Controller & Service)

--- src/projects/project/project.controller.spec.ts
+++ src/projects/project/project.controller.spec.ts
@@ -1,11 +1,43 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProjectController } from './project.controller';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { ProjectService } from './project.service';
+import { TechnologyService } from 'src/technologies/technology/technology.service';
+import { DomainService } from 'src/domains/domain/domain.service';
 
 describe('ProjectController', () => {
   let controller: ProjectController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        JwtService,
+        ConfigService,
+        {
+          provide: TechnologyService,
+          useValue: { getTechnologyByName: jest.fn() },
+        },
+        {
+          provide: ProjectService,
+          useValue: {
+            getProjects: jest.fn(),
+            getSortedProjects: jest.fn(),
+            getProjectById: jest.fn(),
+            createProject: jest.fn(),
+            updateProject: jest.fn(),
+            deleteProject: jest.fn(),
+            addProjectTechnology: jest.fn(),
+            addProjectDomain: jest.fn(),
+          },
+        },
+        {
+          provide: DomainService,
+          useValue: {
+            getDomainByName: jest.fn(),
+          },
+        },
+      ],
       controllers: [ProjectController],
     }).compile();
 
--- src/projects/project/project.service.spec.ts
+++ src/projects/project/project.service.spec.ts
@@ -1,18 +1,59 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProjectService } from './project.service';
+import { JwtService } from '@nestjs/jwt';
+import { Project } from 'src/entities/project';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { TechnologyService } from 'src/technologies/technology/technology.service';
+import { DomainService } from 'src/domains/domain/domain.service';
 
 describe('ProjectService', () => {
   let service: ProjectService;
+  let projectRepository: Repository<Project>;
+  const PROJECT_REPOSITORY_TOKEN = getRepositoryToken(Project);
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ProjectService],
+      providers: [
+        ProjectService,
+        {
+          provide: TechnologyService,
+          useValue: {
+            getTechnologyByName: jest.fn(),
+          },
+        },
+        {
+          provide: DomainService,
+          useValue: {
+            getDomainByName: jest.fn(),
+          },
+        },
+        JwtService,
+        {
+          provide: PROJECT_REPOSITORY_TOKEN,
+          useValue: {
+            find: jest.fn(),
+            findOne: jest.fn(),
+            create: jest.fn(),
+            save: jest.fn(),
+            update: jest.fn(),
+            delete: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<ProjectService>(ProjectService);
+    projectRepository = module.get<Repository<Project>>(
+      PROJECT_REPOSITORY_TOKEN,
+    );
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('projectRepository should be defined', () => {
+    expect(projectRepository).toBeDefined();
   });
 });

f1dfe1a443749f695d4c2bc1e35c14f7d2dda67f
adnane <adnanbenzo194@gmail.com>
2023-05-18 23:02:24 +0100

    Updated resumes module unit tests to handle dependencies (Service & Controller)

--- src/resumes/resume/resume.controller.spec.ts
+++ src/resumes/resume/resume.controller.spec.ts
@@ -1,11 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ResumeController } from './resume.controller';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { ResumeService } from './resume.service';
 
 describe('ResumeController', () => {
   let controller: ResumeController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        JwtService,
+        ConfigService,
+        {
+          provide: ResumeService,
+          useValue: {
+            getResumes: jest.fn(),
+            getResumeById: jest.fn(),
+            createResume: jest.fn(),
+            updateResume: jest.fn(),
+            deleteResume: jest.fn(),
+            setResumeDomain: jest.fn(),
+          },
+        },
+      ],
       controllers: [ResumeController],
     }).compile();
 
--- src/resumes/resume/resume.service.spec.ts
+++ src/resumes/resume/resume.service.spec.ts
@@ -1,18 +1,50 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ResumeService } from './resume.service';
+import { Repository } from 'typeorm';
+import { Resume } from 'src/entities/resume';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { JwtService } from '@nestjs/jwt';
+import { DomainService } from 'src/domains/domain/domain.service';
 
 describe('ResumeService', () => {
   let service: ResumeService;
+  let resumeRepository: Repository<Resume>;
+  const RESUME_REPOSITORY_TOKEN = getRepositoryToken(Resume);
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ResumeService],
+      providers: [
+        ResumeService,
+        JwtService,
+        {
+          provide: RESUME_REPOSITORY_TOKEN,
+          useValue: {
+            find: jest.fn(),
+            findOne: jest.fn(),
+            create: jest.fn(),
+            save: jest.fn(),
+            update: jest.fn(),
+            delete: jest.fn(),
+          },
+        },
+        {
+          provide: DomainService,
+          useValue: {
+            getDomainByName: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<ResumeService>(ResumeService);
+    resumeRepository = module.get<Repository<Resume>>(RESUME_REPOSITORY_TOKEN);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('resumeRepository should be defined', () => {
+    expect(resumeRepository).toBeDefined();
   });
 });

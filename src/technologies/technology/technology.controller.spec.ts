280f80e0ad1648114abbb2e6821b04526c3f2f7c
adnane <adnanbenzo194@gmail.com>
2023-05-19 08:25:15 +0100

    Updated technology module unit tests to handle dependencies (Service & Controller)

--- src/technologies/technology/technology.controller.spec.ts
+++ src/technologies/technology/technology.controller.spec.ts
@@ -1,11 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TechnologyController } from './technology.controller';
+import { TechnologyService } from './technology.service';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 describe('TechnologyController', () => {
   let controller: TechnologyController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        JwtService,
+        ConfigService,
+        {
+          provide: TechnologyService,
+          useValue: {
+            getTechnologyByName: jest.fn(),
+            getTechnologies: jest.fn(),
+            getTechnologyById: jest.fn(),
+            createTechnology: jest.fn(),
+            updateTechnology: jest.fn(),
+            deleteTechnology: jest.fn(),
+          },
+        },
+      ],
       controllers: [TechnologyController],
     }).compile();
 
--- src/technologies/technology/technology.service.spec.ts
+++ src/technologies/technology/technology.service.spec.ts
@@ -1,18 +1,43 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TechnologyService } from './technology.service';
+import { Repository } from 'typeorm';
+import { Technology } from 'src/entities/technology';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
 describe('TechnologyService', () => {
   let service: TechnologyService;
+  let technologyRepository: Repository<Technology>;
+  const TECHNOLOGY_REPOSITORY_TOKEN = getRepositoryToken(Technology);
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TechnologyService],
+      providers: [
+        TechnologyService,
+        {
+          provide: TECHNOLOGY_REPOSITORY_TOKEN,
+          useValue: {
+            find: jest.fn(),
+            findOne: jest.fn(),
+            create: jest.fn(),
+            save: jest.fn(),
+            update: jest.fn(),
+            delete: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<TechnologyService>(TechnologyService);
+    technologyRepository = module.get<Repository<Technology>>(
+      TECHNOLOGY_REPOSITORY_TOKEN,
+    );
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('technologyRepository should be defined', () => {
+    expect(technologyRepository).toBeDefined();
   });
 });

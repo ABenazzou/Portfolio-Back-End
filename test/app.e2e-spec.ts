67b7f5973ad6c2e2b45cbd2baec68448f1151c7c
adnane <adnanbenzo194@gmail.com>
2023-05-20 13:46:34 +0100

    Added initial e2e tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -3,22 +3,37 @@ import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
-describe('AppController (e2e)', () => {
-  let app: INestApplication;
+let app: INestApplication;
+beforeAll(async () => {
+  const moduleFixture: TestingModule = await Test.createTestingModule({
+    imports: [AppModule],
+  }).compile();
+  app = moduleFixture.createNestApplication();
+  await app.init();
+});
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
+afterAll(() => {
+  app.close();
+});
 
-    app = moduleFixture.createNestApplication();
-    await app.init();
+describe('Root route controller (e2e)', () => {
+  it('/ (GET)', () => {
+    return request(app.getHttpServer()).get('/').expect(404);
+  });
+});
+
+describe('Domain route controller (e2e)', () => {
+  it('/api/domain/1 (DELETE)', () => {
+    return request(app.getHttpServer()).delete('/api/domain/1').expect(401);
   });
 
-  it('/ (GET)', () => {
+  it('/api/domain (POST)', () => {
     return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+      .post('/api/domain')
+      .send({
+        name: 'Web Development',
+        logo: 'src/logos/web-development.png',
+      })
+      .expect(401);
   });
 });

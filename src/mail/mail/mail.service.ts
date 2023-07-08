48adf15812e9f0e1f166317a819903e7f3d52615
adnane <adnanbenzo194@gmail.com>
2023-07-09 00:35:49 +0100

    Cleanup mail service

--- src/mail/mail/mail.service.ts
+++ src/mail/mail/mail.service.ts
@@ -1,18 +1,12 @@
 import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { SendEmailDto } from 'src/dtos/emails.dtos';
 
 @Injectable()
 export class MailService {
-    constructor(private mailerService: MailerService, private configService: ConfigService) { }
+    constructor(private mailerService: MailerService ) { }
 
     async sendEmail(sendEmailDto: SendEmailDto) {
-        console.log(sendEmailDto);
-        console.log(this.configService.get('MAIL_PASSWORD'));
-        console.log(this.configService.get('MAIL_USER'));
-        console.log(this.configService.get('MAIL_HOST'));
-        console.log(this.configService.get('DATABASE_HOST'));
         if (sendEmailDto.isCopy) {
             await this.mailerService.sendMail({
                 to: 'adnanbenzo194@gmail.com',

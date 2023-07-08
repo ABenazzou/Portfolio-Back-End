51e4847ca3cda56859c8967e437be9d84127bdbd
adnane <adnanbenzo194@gmail.com>
2023-07-08 17:17:24 +0100

    Hotfix debug prod smtp

--- src/mail/mail/mail.service.ts
+++ src/mail/mail/mail.service.ts
@@ -1,16 +1,17 @@
 import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { SendEmailDto } from 'src/dtos/emails.dtos';
 
 @Injectable()
 export class MailService {
-    constructor(private mailerService: MailerService) { }
+    constructor(private mailerService: MailerService, private configService: ConfigService) { }
 
     async sendEmail(sendEmailDto: SendEmailDto) {
         console.log(sendEmailDto);
-        console.log(process.env.MAIL_PASSWORD);
-        console.log(process.env.MAIL_EMAIL);
-        console.log(process.env.MAIL_HOST);
+        console.log(this.configService.get('MAIL_PASSWORD'));
+        console.log(this.configService.get('MAIL_USER'));
+        console.log(this.configService.get('MAIL_HOST'));
         if (sendEmailDto.isCopy) {
             await this.mailerService.sendMail({
                 to: 'adnanbenzo194@gmail.com',
